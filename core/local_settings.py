bf694009571f245d76477929d4226f7ca81d68b3
Myguel Ângelo <angelo.nunes576@gmail.com>
2024-10-21 05:14:11 +0000

    Alteração de settings no servidor

--- core/local_settings.py
+++ core/local_settings.py
@@ -1,24 +1,24 @@
-# # type: ignore
-# # flake8: noqa
+# type: ignore
+# flake8: noqa
 
-# # Comando:
-# # python -c "import string as s;from secrets import SystemRandom as SR;print(''.join(SR().choices(s.ascii_letters + s.digits + s.punctuation, k=64)));"
-# SECRET_KEY = '1z2jrMODK#2UL}x7|_JNJdrDs`N[PD}(OWxbb1/I<H>(ai#!]Gimwyg(3YM6]CG'
+# Comando:
+# python -c "import string as s;from secrets import SystemRandom as SR;print(''.join(SR().choices(s.ascii_letters + s.digits + s.punctuation, k=64)));"
+SECRET_KEY = '1z2jrMODK#2UL}x7|_JNJdrDs`N[PD}(OWxbb1/I<H>(ai#!]Gimwyg(3YM6]CG'
 
-# # DEBUG DEVE SER False em produção
-# DEBUG = False
+# DEBUG DEVE SER False em produção
+DEBUG = False
 
-# # Seu domínio ou IP devem vir aqui
-# ALLOWED_HOSTS = ['34.45.100.29', 'mappacfo.com.br', 'www.mappacfo.com.br']
+# Seu domínio ou IP devem vir aqui
+ALLOWED_HOSTS = ['34.45.100.29', 'mappacfopm.com.br', 'www.mappacfopm.com.br', '127.0.0.1', 'localhost']
 
-# # Config para postgresql
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'anotacao_cfo',
-#         'USER': 'usuario_mappa',
-#         'PASSWORD': 'senha_database',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
+# Config para postgresql
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'anotacao_cfo',
+        'USER': 'usuario_mappa',
+        'PASSWORD': 'senha_database',
+        'HOST': '127.0.0.1',
+        'PORT': '5433',
+    }
+}
--- core/settings.py
+++ core/settings.py
@@ -126,3 +126,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from core.local_settings import *
+except ImportError:
+    ...
